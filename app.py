b1b7dd4249ae7dd1b1bcda3d291bdbcfb01ad5d5
nelliemckesson <nellie.mckesson@macmillan.com>
2015-07-06 10:47:30 -0400

    very basic stuff

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import os
 import urlparse
+import redis
+import json
 
 from dropbox.client import DropboxClient, DropboxOAuth2Flow
 from flask import abort, Flask, redirect, render_template, request, session, url_for
 
 # App key and secret from the App console (dropbox.com/developers/apps)
-APP_KEY = os.environ['APP_KEY']
-APP_SECRET = os.environ['APP_SECRET']
+APP_KEY = 'ac6gg5ixxf23stg'
+APP_SECRET = 'ck8zgw5hreorljp'
 
 app = Flask(__name__)
-app.config['DEBUG'] = os.environ['DEBUG'] == 'True'
+app.config['DEBUG'] = True
 
 # A random secret used by Flask to encrypt session data cookies
-app.secret_key = os.environ['FLASK_SECRET_KEY']
+app.secret_key = 'gibberishtktktk'
+
+redis_client = redis.from_url('localhost:6379')
 
 def get_url(route):
     '''Generate a proper URL, forcing HTTPS if not running locally'''
@@ -44,9 +48,49 @@ def oauth_callback():
     '''Callback function for when the user returns from OAuth.'''
     access_token, uid, extras = get_flow().finish(request.args)
     client = DropboxClient(access_token)
+    redis_client.hset('tokens', uid, access_token)
 
-    return render_template('done.html', display_name=client.account_info()['display_name'])
+    return render_template('done.html', display_name=uid)
+
+
+@app.route('/newfolder/<uid>')
+def newfolder(uid):
+    #278097779
+    access_token = redis_client.hget('tokens', uid)
+    client = DropboxClient(access_token)
+    all_data = client.metadata('/')
+    contents = all_data['contents']
+    folders = []
+    for item in all_data['contents']:
+      for key in item:
+        if key == 'is_dir':
+          if item[key] == True:
+            for path, value in item.iteritems():
+              if path == 'path':
+                folders.append(value)
+    display_folders = "<br />".join(str(folder) for folder in folders)
+    return render_template('newfolder.html', display_folders=display_folders, uid=uid)
+
+
+# @app.route('/newfolder/<uid>')
+# def newfolder(uid):
+#     #278097779
+#     access_token = redis_client.hget('tokens', uid)
+#     client = DropboxClient(access_token)
+#     return render_template('newfolder.html', uid=uid)
+
+
+@app.route('/newfolder/createnewfolder/<uid>')
+def createnewfolder(uid):
+    access_token = redis_client.hget('tokens', uid)
+    client = DropboxClient(access_token)
+    newfolder = client.file_create_folder('APITestFolder2')
+    return str(newfolder)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+#How do I return my string of folder names AND an html template? > jinja, use FOR loop in the templting engine; flask tutorial
+#How do I pass in the UID without calling it in the url every time? > store uid in session
+#How do I POST the folder creation?
